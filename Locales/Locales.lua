7aabccc31a2d531e205f858adc30f9e682cbb7e5
Sacred Duckwhale <duckwhale@mail.ru>
2017-10-05 16:40:16 +0200

    Initial commit

---
+++ Locales/Locales.lua
@@ -0,0 +1,13 @@
+local L;
+L = LibStub("AceLocale-3.0"):NewLocale("ContainerLootLogger", "enGB", true);
+
+L["Used spell %s on target item %s - %s x%d"] = true;
+L["At this point, no slash commands have been implemented. Sorry about that!"] = true;
+L["All data has been reset."] = true;
+L["Data for spell %s has been reset."] = true;
+L["Data for spell %s on target %s has been reset."] = true;
+L["Looted %s from %s: %s"] = true;
+L["Looted %s from %s: %d x %s"] = true;
+L["Received loot from %s: %d x %s"] = true;
+L["Debug mode enabled."] = true;
+L["Debug mode disabled."] = true;
