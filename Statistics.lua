6fd8cabc4408c16935c47819491ff76ea5df31a7
Sacred Duckwhale <duckwhale@mail.ru>
2018-02-18 14:05:56 +0100

    Added /cll gold slash command for displaying OH gold statistics

--- Statistics.lua
+++ Statistics.lua
@@ -12,3 +12,45 @@
     -- You should have received a copy of the GNU General Public License
     -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ----------------------------------------------------------------------------------------------------------------------
+
+
+local addonName, CLL = ...
+if not CLL then return end
+
+
+-- Initialise environment
+local Statistics = {}
+
+-- Locals and constants
+local MODULE = "Statistics"
+
+-- Upvalues
+local L = CLL.L
+local ChatMsg = CLL.Output.Print
+local DebugMsg = CLL.Debug.Print
+local tostring = tostring
+local format = format
+local GetCoinTextureString = GetCoinTextureString
+
+
+-- Print a formatted message to display the gained Gold from Legion Order Halls
+function Statistics.PrintGold(fqcn) -- TODO: Command for specific items of interest (PS, BOS, etc - and track resource consumption?), stats for ALL characters, by server, etc.
+
+	fqcn = fqcn or CLL.GetFQCN()
+	ChatMsg(format("Gold summary for %s:", tostring(fqcn))) -- TODO: L
+	
+	local db = ContainerLootLoggerDB[fqcn]
+	if not db then
+		DebugMsg(MODULE, "Failed to print summary because a DB entry for this character doesn't exist")
+		return
+	end
+	
+	local goldValue = db["LEGION_ORDER_HALL"] and db["LEGION_ORDER_HALL"]["GOLD"] and db["LEGION_ORDER_HALL"]["GOLD"]["amount"] or 0
+	
+	ChatMsg("Total: " .. GetCoinTextureString(goldValue)) -- TODO: L / different stats for each day (DAY/WEEK/TOTAL - AVG, MIN, MAX, STDEV etc)
+	
+end
+
+
+-- Add module to shared environment
+CLL.Statistics = Statistics
